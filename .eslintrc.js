0446bedca6d3f562fba8d8cc722550f976abad3b
misaosyushi <misaki.tsujita@rakus-partners.co.jp>
2020-08-28 15:56:02 +0900

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  plugins: ['prettier', '@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  env: { node: true, es6: true },
+  parserOptions: {
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    'prettier/prettier': ['error', { printWidth: 120 }],
+  },
+}
--- domain/Film/Film.ts
+++ domain/Film/Film.ts
@@ -1,9 +1,9 @@
 export class Film {
-    readonly episode_id: number
-    readonly title: string
+  readonly episode_id: number
+  readonly title: string
 
-    constructor(episodeId: number, title: string) {
-        this.episode_id = episodeId
-        this.title = title
-    }
+  constructor(episodeId: number, title: string) {
+    this.episode_id = episodeId
+    this.title = title
+  }
 }
